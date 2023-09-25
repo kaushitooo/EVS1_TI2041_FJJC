d57014de914e9b45009e451b9b50133cc94f6055
kaushito <kaushoficial@gmail.com>
2023-09-25 15:09:42 -0300

    proyecto rama2 terminada

---
+++ fjjc_app2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FjjcApp2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fjjc_app2'
--- fjjc_proyecto1/settings.py
+++ fjjc_proyecto1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "fjjc_app2",
 ]
 
 MIDDLEWARE = [
